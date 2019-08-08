85e50b985e13c2d4e480524e81597b505532e40d
Guilherme Peres <guilherme.vperes@gmail.com>
2019-08-07 23:24:29 -0300

    imagens e correcao cors

--- src/controllers/PlateController.js
+++ src/controllers/PlateController.js
@@ -1,4 +1,7 @@
 const Plate = require('../models/Plate')
+const sharp = require('sharp')
+const path = require('path')
+const fs = require('fs')
 
 module.exports = {
 
@@ -10,11 +13,22 @@ module.exports = {
   async store (req, res) {
     const { name } = req.body
     const { filename: image } = req.file
-    console.log(req.file)
+
+    const [name] = image.split('.')
+    const fileName = `${name}.jpg`
+
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(
+        path.resolve(req.file.destination, 'resized', image)
+      )
+
+    // fs.unlinkSync(req.file.pa) // deleta a imagem de uploads
 
     const plate = await Plate.create({
       name,
-      image
+      image: fileName
     })
 
     return res.json(plate)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const cors = require('cors')
 
 const app = express()
@@ -8,6 +9,10 @@ mongoose.connect('mongodb+srv://gvperes:134679@cluster0-giyyc.mongodb.net/test?r
   useNewUrlParser: true
 })
 
+app.use(cors())
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+
 app.use(require('./routes'))
 
 app.use(function (req, res, next) {
@@ -15,7 +20,5 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-
-app.use(cors())
 
 app.listen(3040)
